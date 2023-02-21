fa60ae48d2e8c649536986f8e8703ad25b434820
Xuehai <xhdong@aceinna.com>
2023-02-21 16:42:48 +0800

    feat: improve PR #6

--- test/rtk2go.test.js
+++ test/rtk2go.test.js
@@ -3,12 +3,12 @@ const { NtripClient } = require('../index');
 const options = {
   host: 'rtk2go.com',
   port: 2101,
-  mountpoint: 'ABIOS',
-  username: 'test',
+  mountpoint: 'ACACU',
+  username: 'test@test.com',
   password: 'test',
   xyz: [-1983430.2365, -4937492.4088, 3505683.7925],
   // the interval of send nmea, unit is millisecond
-  interval: 2000
+  interval: 2000,
 };
 
 const client = new NtripClient(options);
@@ -26,3 +26,9 @@ client.on('error', (err) => {
 });
 
 client.run();
+
+
+setTimeout(() => {
+  client.close();
+}, 10000);
+
